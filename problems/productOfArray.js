fd10a09ec4e42e98352420db7a78b51124292b00
jai84322 <jai84322@gmail.com>
2022-05-09 19:49:33 +0530

    array-methods

--- problems/productOfArray.js
+++
@@ -1,8 +0,0 @@
-// ================================= product of array of numbers ==============================================
-let number = [1,2,3,4,5]
-let product = 1;
-for (i = 0; i<number.length; i++){
-    product  = product * number[i]
-    console.log(product)
-}
-console.log(product)
--- problems/sumOfArray.js
+++
@@ -1,8 +0,0 @@
-// ================================= sum of array of numbers ==============================================
-let number = [1,2,3,4,5]
-let sum=0;
-for (i = 0; i<number.length; i++){
-    sum  = sum + number[i]
-    console.log(sum)
-}
-console.log(sum)
