bdf54779cb93e61f7ebcefcaa74a90052c0e7575
jai84322 <jai84322@gmail.com>
2022-05-09 19:42:40 +0530

    problems

---
+++ problems/sumOfArray.js
@@ -0,0 +1,8 @@
+// ================================= sum of array of numbers ==============================================
+let number = [1,2,3,4,5]
+let sum=0;
+for (i = 0; i<number.length; i++){
+    sum  = sum + number[i]
+    console.log(sum)
+}
+console.log(sum)
