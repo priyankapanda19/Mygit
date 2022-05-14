a2134646716d21674db8e834064b77fcdeb19ec0
jai84322 <jai84322@gmail.com>
2022-05-14 20:46:59 +0530

    push-pop-added

---
+++ pop.js
@@ -0,0 +1,16 @@
+// let numbers = [1,2,3]
+
+// numbers.pop()
+
+// console.log(numbers);
+
+// it removes the last element of the array by default
+
+// ================ get the removed item which has been removed =============
+
+let numbers = [1,2,3]
+
+let lastItem = numbers.pop() // this variable stores the last item which is 3 on index 2
+
+console.log(numbers);
+console.log(lastItem);
---
+++ push.js
@@ -0,0 +1,16 @@
+// let numbers = [1,2,3,4,5]
+
+// numbers.push(6,7,8,9,10)
+
+// console.log(numbers);
+
+// [1, 2, 3, 4,  5, 6, 7, 8, 9, 10]
+
+// ================= it also returns the legth of the array =============
+
+let numbers = [1,2,3,4,5]
+
+let total = numbers.push(6,7,8,9,10)
+
+console.log(numbers);
+console.log(total); // this will return the length of the array which is 10
