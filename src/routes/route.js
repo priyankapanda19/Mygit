1f6b06ea9581e6977fe27dfd90b2a19610e6d1c8
priyanka <julypriyanka59@gmail.com>
2022-08-09 00:00:48 +0530

    get-api

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,54 @@
 const express = require('express');
-const abc = require('../introduction/intro');
-const { call1 } = require('../logger/logger');
-const { call4 } = require('../util/helper');
-const { call2 } = require('../validator/formatter');
-const router = express.Router();
+// const abc = require('../introduction/intro');
+// const { call1 } = require('../logger/logger');
+// const { call4 } = require('../util/helper');
+// const { call2 } = require('../validator/formatter');
+ const router = express.Router();
+ const movie=['Don','Devdas','Mela','Sanam Teri Kasam','Dangal','zid']
+router.get('/movie', function (req, res) {
+    // console.log('My batch is', abc.name)
+    // abc.printName()
+    // call1.welCome()
+    // call2.greeting()
+    // call3.date()
+    // call4.nameOfMonth()
+    // res.send('My second ever api!')
 
-router.get('/test-me', function (req, res) {
-    console.log('My batch is', abc.name)
-    abc.printName()
-    call1.welCome()
-    call2.greeting()
-    call3.date()
-    call4.nameOfMonth()
-    res.send('My second ever api!')
+    
+    res.send(movie)
 });
+
+router.get('/movie/:indexNumber', function (req, res) {
+
+   let i=req.params.indexNumber
+   let j=i-1
+   if(i<=movie.length){
+    res.send(movie[j])
+   } else{res.send("Invalid Index")}
+    
+});
+
+router.get('/films', function (req, res) {
+   const films=[{
+     id: 1,
+    name: "The Shining"
+   }, {
+    id: 2,
+    name: "Incendies"
+   }, {
+    id: 3,
+    name: "Rang de Basanti"
+   }, {
+    id: 4,
+    name: "Finding Nemo"
+}]
+    res.send(films)
+
+   })
+   
+
+
+
 
 
 router.get('/test-you', function(req, res){
