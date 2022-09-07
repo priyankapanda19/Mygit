d41cf86081a3fb288ef1853c943e75f032dde32b
Amar yadav_7878355926 <105384661+AMAR7878@users.noreply.github.com>
2022-09-07 22:51:22 +0530

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt=require("jsonwebtoken")
-const userModel=require("../Models/authorModel")
+const blogModel = require("../models/blogModel");
 
 
 //-------------------------------authentication-----------------------------//
