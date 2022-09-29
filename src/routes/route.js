663517a4b1c0227627755aa5cf74352fece65ee5
priyanka <julypriyanka59@gmail.com>
2022-09-29 08:15:10 +0530

    add aws

--- src/routes/route.js
+++ src/routes/route.js
@@ -11,7 +11,7 @@ const aws= require("aws-sdk")
 //--------------------adding aws---------------------------//
 aws.config.update({
    accessKeyId: "AKIAY3L35MCRZNIRGT6N",
-   secretAccessKeyId: "9f+YFBVcSjZWM6DG9R4TUN8k8TGe4X+lXmO4jPiU",
+   secretAccessKey: "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",
    region: "ap-south-1"
 })
 
